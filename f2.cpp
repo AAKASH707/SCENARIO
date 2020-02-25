64740cf877b3d3a870d6d8640b173413893d02b5
Lucifer <lucifer6218@gmail.com>
2020-02-25 16:13:41 +0530

    Code for stater's in programing world

---
+++ f2.cpp
@@ -0,0 +1,12 @@
+/*
+ THIS IS CODE IN CPP FOR FRIST TIME USER*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<< "Hello world";
+	return 0;
+}
+
+
---
+++ f3.c
@@ -0,0 +1,9 @@
+/*THIS IS A CODE IN C FOR THE FRIST TIME USER*/
+#include<stdio.h>
+int main()
+{
+	printf("Hello world");
+	return 0;
+
+}
+
