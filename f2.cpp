96d0e42fe90779bb4c013225cac045e49e14b106
Lucifer <lucifer6218@gmail.com>
2020-02-25 17:07:40 +0530

    modification done in 17 th line -->float

--- f2.cpp
+++ f2.cpp
@@ -8,5 +8,19 @@ int main()
 	cout<< "Hello world";
 	return 0;
 }
+*
+*
+* #include<iostream>
+*using namespace std;
+int main()
+{
+	float a,b,c;
+	cout<< "Enetr value of two number ;"<<endl;
+	cin>>a>>b;
+	c=a+b;
+	cout<< "Result is:"<< c<<endl;
+	return 0;
+
+}
 
 
